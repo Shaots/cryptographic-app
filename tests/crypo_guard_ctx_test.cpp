0edc7cc1b1f9e9e0daf0fe40b0f412e70f7c5213
Roman <roman.miv@yandex.ru>
2025-08-20 16:16:36 +0000

    [u] Update crypo_guard_ctx_test -- add test checksum

--- tests/crypo_guard_ctx_test.cpp
+++ tests/crypo_guard_ctx_test.cpp
@@ -48,7 +48,7 @@ TEST(TestCrypt, encryptDecryptLongMsg) {
     std::string msg;
     size_t len = 10'000'000;
     msg.reserve(len);
-    for(size_t i = 0; i < len; ++i) {
+    for (size_t i = 0; i < len; ++i) {
         msg += 'a' + (i % 26);
     }
 
@@ -63,4 +63,56 @@ TEST(TestCrypt, encryptDecryptLongMsg) {
     std::stringstream deOutStream;
     EXPECT_NO_THROW(cryptoCtx.DecryptFile(deInStream, deOutStream, "1234567"));
     EXPECT_EQ(deOutStream.str(), msg);
+}
+
+// 5: Checksum
+TEST(TestCrypt, checksum) {
+    std::string msg("this is input");
+    std::stringstream inStream(msg);
+
+    CryptoGuard::CryptoGuardCtx cryptoCtx;
+    std::string checksum;
+    EXPECT_NO_THROW(checksum = cryptoCtx.CalculateChecksum(inStream));
+    EXPECT_EQ(checksum.length(), 64);
+}
+
+// 6: Checksum with long message
+TEST(TestCrypt, checksumLongMsg) {
+    std::string msg;
+    size_t len = 2'000'000;
+    msg.reserve(len);
+    for (size_t i = 0; i < len / 2; ++i) {
+        msg += 'a' + (i % 26);
+    }
+    for (size_t i = len / 2; i < len; ++i) {
+        msg += 'A' + (i % 26);
+    }
+    std::stringstream inStream(msg);
+
+    CryptoGuard::CryptoGuardCtx cryptoCtx;
+    std::string checksum;
+    EXPECT_NO_THROW(checksum = cryptoCtx.CalculateChecksum(inStream));
+    EXPECT_EQ(checksum.length(), 64);
+}
+
+// 7: Checksum + Encrypt + Decrypt --> Checksum
+TEST(TestCrypt, checkEncDec) {
+    std::string msg("this is input");
+    std::stringstream inStream(msg);
+
+    CryptoGuard::CryptoGuardCtx cryptoCtx;
+    std::string checksum;
+    EXPECT_NO_THROW(checksum = cryptoCtx.CalculateChecksum(inStream));
+    EXPECT_EQ(checksum.length(), 64);
+
+    std::string password("1234567");
+    std::stringstream inStream_(checksum);
+    std::stringstream outStream;
+
+    EXPECT_NO_THROW(cryptoCtx.EncryptFile(inStream_, outStream, password));
+
+    std::stringstream deInStream(outStream.str());
+    std::stringstream deOutStream;
+    EXPECT_NO_THROW(cryptoCtx.DecryptFile(deInStream, deOutStream, "1234567"));
+    EXPECT_EQ(deOutStream.str(), checksum);
 }
